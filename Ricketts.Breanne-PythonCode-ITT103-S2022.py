c5741f45b56eaec9f60cf5918bd6c01026163576
BreanneRicketts <104812199+BreanneRicketts@users.noreply.github.com>
2022-05-03 20:31:28 -0500

    Add files via upload

---
+++ Ricketts.Breanne-PythonCode-ITT103-S2022.py
@@ -0,0 +1,41 @@
+
+
+Class_1c_rate = 0.06
+Class_1d_rate = 0.07
+Class_1e_rate = 0.10
+
+Class_2c_rate = 0.04
+Class_2d_rate = 0.06
+
+Class_3_rate = 0.045
+
+Salesperson_num= int(input("Input salesperson number"))
+while Salesperson_num != 0:
+    Salesperson_num= int(input("Input salesperson number"))
+    Sales_amount=int(input("Input Sales amount"))
+    Class=int(input("Input Class"))
+    if Class == 1:
+        if Sales_amount <= 1000:
+            print ("Your commission rate is", Sales_amount * Class_1c_rate)
+    if Class == 1:
+        if Sales_amount > 1000 and Sales_amount < 2000:
+            print("Your comission rate is",Sales_amount * Class_1c_rate)
+    if Class == 1:
+        if Sales_amount >= 2000:
+            print ("Your comission rate is", Sales_amount * Class_1e_rate)
+    elif Class == 2:
+        if Sales_amount < 1000:
+            print ("Your comission rate is", Sales_amount * Class_2c_rate)
+    if Class == 2:
+        if Sales_amount >= 1000:
+            print ("Your comission rate is", Sales_amount * Class_2c_rate)
+    elif Class == 3:
+        print("Your commision rate is", Sales_amount * Class_3_rate)
+    else:
+        print("Error Message")
+
+    i=input("Would you like to continue? ")
+    if i =="no":
+        break
+
+"End"
